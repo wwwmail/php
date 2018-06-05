6d7a4cd5f1c1a05cc611a701f7ee5de93070896b
user2 user2 <root@mail.md>
2018-06-05 14:06:27 +0300

    change

--- task1/index.php
+++ task1/index.php
@@ -8,10 +8,8 @@ $files = get_files_list(DOWNLOAD_DIR);
 
 
 if(isset($_FILES['file_name'])){
-
  
         $info =  add_file($_FILES['file_name']);
-
 }
 
 if(isset($_POST['del_file']) && isset($_POST['file_name_for_del'])){
