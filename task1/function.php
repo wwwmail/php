c61e31b0c1da2e8eb90cc6228cb0d439a054db36
dev dev <www@mail.cz>
2018-06-06 20:30:40 +0300

    1

--- task1/function.php
+++ task1/function.php
@@ -12,128 +12,126 @@ function change_permissions($file_path, $permission = '0644'){
 
 
 function check_max_upload_size($size){
-    
-    
-    if($size > 2000000){
-        return false;
-    }elseif ($size == 0) {
-        return false;
-    }else{
-        return true;
-    }
-      
+
+
+        if($size > 2000000){
+                return false;
+        }elseif ($size == 0) {
+                return false;
+        }else{
+                return true;
+        }
 
 }
 
 
 function check_downloads_dir($path_to_dir){
- 
-if(substr(sprintf('%o', fileperms($path_to_dir)), -4) == '0777'){
 
-        return true;
-    }else{
-        return PERMISSION_DIR;
-    }
+        if(substr(sprintf('%o', fileperms($path_to_dir)), -4) == '0777'){
+
+                return true;
+        }else{
+                return PERMISSION_DIR;
+        }
 
 }
 
 function check_is_can_del($path){
 
 
-    if(is_writable($path)){
+        if(is_writable($path)){
 
-        return true;
-    }else{
-        return PERMISSION_FILE;
-    }
+                return true;
+        }else{
+                return PERMISSION_FILE;
+        }
 }
 
 
-
+    
 function check_is_readable($path){
 
         if (is_readable($path)){
-            return true;
+                return true;
         }else{
-            return READABLE_DIR;
+                return READABLE_DIR;
         }
 }
 
 
 function get_files_list($dir){
 
-    $return_array = [];
-    
-      if (true === check_downloads_dir($dir)) {
-            
-        $directory = new DirectoryIterator($dir);
-        $i = 0;
-        foreach ($directory as $fileinfo) {
+        $return_array = [];
 
-            if ($fileinfo->isDot())
-                continue;
-            $i++;
-            $return_array[] = [
-                'name' => "$fileinfo",
-                'size' => filesize_formatted($fileinfo->getSize()),
-                'number' => $i,
-                'path_to_file' => $dir . "/" . $fileinfo,
-            ];
+        if (true === check_downloads_dir($dir)) {
+
+                $directory = new DirectoryIterator($dir);
+                $i = 0;
+                foreach ($directory as $fileinfo) {
+
+                        if ($fileinfo->isDot())
+                                continue;
+                        $i++;
+                        $return_array[] = [
+                                'name' => "$fileinfo",
+                                'size' => filesize_formatted($fileinfo->getSize()),
+                                'number' => $i,
+                                'path_to_file' => $dir . "/" . $fileinfo,
+                        ];
+                }
+
+
+                return $return_array;
+        }else {
+                return check_downloads_dir($dir);
         }
-
-
-        return $return_array;
-    }else {
-        return check_downloads_dir($dir);
-    }
 }
 
 
 function filesize_formatted($size){
 
-    $units = array( 'B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
-    $power = $size > 0 ? floor(log($size, 1024)) : 0;
-    return number_format($size / pow(1024, $power), 2, '.', ',') . ' ' . $units[$power];
+        $units = array( 'B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB');
+        $power = $size > 0 ? floor(log($size, 1024)) : 0;
+        return number_format($size / pow(1024, $power), 2, '.', ',') . ' ' . $units[$power];
 }
 
 
 function check_file_exist($path){
-    if(file_exists($path)){
-       return FILE_EXIST;
-    }else{
-        return true;
-    }
+        if(file_exists($path)){
+                return FILE_EXIST;
+        }else{
+                return true;
+        }
 
 }
 
 
 function add_file($array){
-   
-    if (true === check_downloads_dir(DOWNLOAD_DIR)) {
-        if (true === check_file_exist(DOWNLOAD_DIR . "/" . $array['name'])) {
-            $uploads_dir = DOWNLOAD_DIR;
-            $name = $array['name'];
-            $tmp_name = $array['tmp_name'];
-            //var_dump($array); die;
-            if(check_max_upload_size($array['size'])){
+
+        if (true === check_downloads_dir(DOWNLOAD_DIR)) {
+                if (true === check_file_exist(DOWNLOAD_DIR . "/" . $array['name'])) {
+                        $uploads_dir = DOWNLOAD_DIR;
+                        $name = $array['name'];
+                        $tmp_name = $array['tmp_name'];
+                        //var_dump($array); die;
+                        if(check_max_upload_size($array['size'])){
 
 
-            if( move_uploaded_file($tmp_name, "$uploads_dir/$name")){
-                    change_permissions("$uploads_dir/$name", 0777);
-                  return SUCCESS;
-            }
+                                if( move_uploaded_file($tmp_name, "$uploads_dir/$name")){
+                                        change_permissions("$uploads_dir/$name", 0777);
+                                        return SUCCESS;
+                                }
 
-            }else{
-                return MAX_UPLOAD_FILE;
-            }
-           // change_permissions("$uploads_dir/$name", 0777);
+                        }else{
+                                return MAX_UPLOAD_FILE;
+                        }
 
+                } else {
+                        return check_file_exist(DOWNLOAD_DIR . "/" . $array['name']);
+                }
         } else {
-            return check_file_exist(DOWNLOAD_DIR . "/" . $array['name']);
+                return check_downloads_dir(DOWNLOAD_DIR);
         }
-    } else {
-        return check_downloads_dir(DOWNLOAD_DIR);
-    }
 }
 
 
@@ -141,7 +139,7 @@ function delete_file($path){
         if(true === check_is_can_del($path)){
 
                 if(unlink($path)){
-                    return SUCCESS;
+                        return SUCCESS;
                 }
         }else{
                 return check_is_can_del($path);
