29c86e5f5218e5f98281997858e46ac3950ab13e
user2 user2 <root@mail.md>
2018-06-09 17:01:07 +0300

    t

--- task3/index.php
+++ task3/index.php
@@ -3,26 +3,37 @@ include ('config.php');
 include ('libs/File.php');
 
 
-
-
+//
+//$a = 'test';
+//if(isset($a[5]))
+//var_dump($a[5]);
+//die;
 
 
 
 $obj = new File();
 
-$obj->setFileContent('text.txt');
+$obj->setFileContent('files/text.txt');
+
 
 $fileArray = $obj->getFileContent();
-
 
 echo "<pre>";
 
 
-$string = $obj->getStringFromFile(3);
+//$string = $obj->getStringFromFile(5);
+$string = $obj->getSymbolFromFile(5, 3);
+
+$obj->replaceSymbol(2, 4, 's');
 
 
+$fileArray = $obj->getFileContent();
 
-var_dump($string);
+var_dump($fileArray);
 
+$obj->printFile();
+
+
+//$obj->saveChenges('/var/www/html/domains/test.test/php/task3/files','new.txt');
 
 include('template/index.php');
--- task3/libs/File.php
+++ task3/libs/File.php
@@ -1,30 +1,132 @@
-<?php
+<?php 
 
-class File
+class File 
 {
-    private $fileContent;
-    
 
-    public function setFileContent($file)
+        private $fileContent;
+
+        public function setFileContent(string $filePath)
+        {
+                if(file_exists($filePath)){
+                    $this->fileContent = file($filePath);
+                }
+        }
+        
+        public function getFileContent()
+        {
+            return $this->fileContent;
+        }
+        
+        
+        private function checkNum(int $num)
+        {
+            if(is_int($num) && $num > 0){
+                return true;
+            }else{
+                return false;
+            }
+        }
+        
+        
+        private function isExistString(int $numStr)
+        {
+            if($this->checkNum($numStr)){
+                if(array_key_exists($numStr-1, $this->fileContent)){
+                    return true;
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
+         
+
+
+
+
+        public function  getStringFromFile(int $numStr)
+        {
+            if($this->isExistString($numStr)){
+                return $this->fileContent[$numStr-1];
+            } else {
+                return "error, string not found";
+            }
+            
+        }
+        
+        private function isExistSymbol(int $numStr, int $numSym)
+        {
+            if($this->checkNum($numSym)){
+                if($this->isExistString($numStr) ){
+                    if(isset($this->fileContent[$numSym])){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }else{
+                    return false;
+                }
+            }else{
+                return false;
+            }
+        }
+        
+        public function getSymbolFromFile(int $numStr, int $numSym)
+        {
+            if($this->isExistSymbol($numStr, $numSym)){
+                $string = $this->getStringFromFile($numStr);
+                return $string[$numSym-1];
+            }else{
+                return "symbol not exist";
+            }
+        }
+        
+        
+        public function replaceString(int $numStr, string $str)
+        {
+            if($this->isExistString($numStr)){
+                $this->fileContent[$numStr] = $str;
+                return true;
+            }else{
+                return "string not found";
+            }
+        }
+        
+        public function replaceSymbol(int $numStr, int $numSym, string $symbol)
+        {
+            if($this->isExistSymbol($numStr, $numSym)){
+                $this->fileContent[$numStr-1][$numSym-1] = $symbol;
+              
+                return true;
+            }else{
+                return "symbol not exist";   
+            }
+        }
+        
+    public function printFile(string $filePath)
     {
-        if (is_file($file)){
-                $this->fileContent = file($file);
+        if(file_exists($filePath)){
+            foreach ($this->fileContent as $string){
+                for($i=0; $i < iconv_strlen($string); $i++){
+                    echo $string[$i];
+                }
+            }
+            return true;
+        }else{
+            return 'file not found';
         }
     }
-
-    public function getFileContent()
-    {
-        return $this->fileContent;
-    }
-
-    public function getStringFromFile($numString)
-    {
-        if(array_key_exists((int)$numString)){
-            return $this->fileContet[$numString];
-        }else{
-            return 'Sorry this file didn\'t has the number of string';
-        }     
-    }
     
+    public function saveChenges(string $filePath)
+    {
+        $fp = fopen($filePath, 'w');
+        fwrite($fp, print_r($this->contentFile, TRUE));
+        fclose($fp);
+    }
+        
 
+        
+        
+        
 }
