2c04843fa3a5fab4dbf8aa03732e8d747a7de5dd
user2 user2 <root@mail.md>
2018-06-12 12:20:10 +0300

    t8

---
+++ task8/libs/Controller.php
@@ -0,0 +1,40 @@
+<?php
+
+class Controller
+{
+		private $model;
+		private $view;
+
+		public function __construct()
+		{		
+		    $this->model = new Model();
+			$this->view = new View(TEMPLATE);	
+				
+			if(isset($_POST['email']))
+			{	
+				$this->pageSendMail();
+			}
+			else
+			{
+				$this->pageDefault();	
+			}
+			
+			$this->view->templateRender();			
+	    }	
+		
+		private function pageSendMail()
+		{
+			if($this->model->checkForm() === true)
+			{
+				$this->model->sendEmail();
+			}
+			$mArray = $this->model->getArray();		
+	        $this->view->addToReplace($mArray);	
+		}	
+			    
+		private function pageDefault()
+		{   
+		   $mArray = $this->model->getArray();		
+	       $this->view->addToReplace($mArray);			   
+		}				
+}
