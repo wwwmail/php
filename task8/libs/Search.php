d73e90e5a78ce65cd47767eb986eb2d082a999a5
dev dev <www@mail.cz>
2018-08-14 20:23:32 +0300

    fix t8

--- task8/libs/Search.php
+++ task8/libs/Search.php
@@ -9,16 +9,13 @@ class Search
         $ch = curl_init();
 
         curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-       // curl_setopt($ch, CURLOPT_ENCODING, 'UTF-8');
+        curl_setopt($ch, CURLOPT_HEADER, 0); 
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_URL, "https://www.google.com.ua/search?q=$query&aqs=chrome..69i57j69i60l3j35i39l2.2081j0j7&sourceid=chrome&ie=UTF-8");
-
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);       
 
         $data = curl_exec($ch);
         curl_close($ch);
-
 
         $data = iconv("windows-1251", "utf-8", $data);
         return $data;
@@ -28,8 +25,6 @@ class Search
 
     public function getSearchData($query)
     {
-
-
         $array = array();
         $data  = $this->sendCurl($query);
         
